d25ef209683a239334293ffe58207c930b25f00a
liujie <liujie045.china@outlook.com>
2019-05-31 16:57:58 +0800

    Handy Commit

--- library/src/main/java/com/handy/frame/module/detail/BaseDetailActivity.java
+++ library/src/main/java/com/handy/frame/module/detail/BaseDetailActivity.java
@@ -5,12 +5,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.widget.RelativeLayout;
 
 import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.ObjectUtils;
+import com.handy.adapter.FragmentPagerAdapter2;
 import com.handy.frame.R;
 import com.handy.frame.base.FrameActivity;
 import com.handy.widget.titlebar.HandyTitleBar;
@@ -62,7 +62,7 @@ public abstract class BaseDetailActivity extends FrameActivity {
         }
 
         viewpager.setOffscreenPageLimit(tabItemEntities.size() - 1);
-        viewpager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+        viewpager.setAdapter(new FragmentPagerAdapter2(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int i) {
                 return tabItemEntities.get(i).getFragment();
@@ -94,16 +94,15 @@ public abstract class BaseDetailActivity extends FrameActivity {
             this.fragment = fragment;
         }
 
-
-        public String getTabName() {
+        String getTabName() {
             return tabName;
         }
 
-        public Fragment getFragment() {
+        Fragment getFragment() {
             return fragment;
         }
 
-        public TabLayout.Tab getTabItem(TabLayout tabLayout) {
+        TabLayout.Tab getTabItem(TabLayout tabLayout) {
             return tabLayout.newTab().setText(this.tabName);
         }
     }
