701b4c816ec660d1e0dbb35e97fb30741b586d69
liujie <liujie045.china@outlook.com>
2019-06-03 14:16:48 +0800

    Handy Commit

--- app/src/main/java/com/handy/frame/app/module/detail/DetailActivity.java
+++ app/src/main/java/com/handy/frame/app/module/detail/DetailActivity.java
@@ -1,7 +1,12 @@
 package com.handy.frame.app.module.detail;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.view.Gravity;
+import android.widget.TextView;
 
 import com.handy.basic.utils.IntentUtils;
 import com.handy.frame.app.module.detail.fragments.InfoFragment;
@@ -25,14 +30,44 @@ public class DetailActivity extends BaseDetailActivity {
         IntentUtils.openActivity(activity, DetailActivity.class, isFinish);
     }
 
+    @Override
+    public void initViewHDB(@Nullable Bundle savedInstanceState) {
+        super.initViewHDB(savedInstanceState);
+        setTabLayoutMode(TabLayout.MODE_FIXED);
+    }
+
     @NonNull
     @Override
     protected List<TabItemEntity> setTabItems() {
+        TextView textView0 = new TextView(context);
+        textView0.setText("OOO");
+        textView0.setGravity(Gravity.CENTER);
+        TextView textView1 = new TextView(context);
+        textView1.setText("AAA");
+        textView1.setGravity(Gravity.CENTER);
+        TextView textView2 = new TextView(context);
+        textView2.setText("BBB");
+        textView2.setGravity(Gravity.CENTER);
+        TextView textView3 = new TextView(context);
+        textView3.setText("CCC");
+        textView3.setGravity(Gravity.CENTER);
+        TextView textView4 = new TextView(context);
+        textView4.setText("DDD");
+        textView4.setGravity(Gravity.CENTER);
+        TextView textView5 = new TextView(context);
+        textView5.setText("EEE");
+        textView5.setGravity(Gravity.CENTER);
         return new ArrayList<TabItemEntity>() {{
-            add(new TabItemEntity("基本信息", new InfoFragment()));
-            add(new TabItemEntity("关联资源", new ListFragment()));
-            add(new TabItemEntity("其他信息", new OthersFragment()));
+            add(new TabItemEntity(textView1, textView0, new InfoFragment()));
+            add(new TabItemEntity(textView2, new ListFragment()));
+            add(new TabItemEntity(textView3, new OthersFragment()));
+            add(new TabItemEntity(textView4, textView0, new InfoFragment()));
+            add(new TabItemEntity(textView5, new ListFragment()));
         }};
     }
 
+    @Override
+    protected void onTabSelectedListener(boolean isSelected, TabLayout.Tab tab) {
+
+    }
 }
