fb5c4d4bd1b14f4a4a8a271198d3f15e79a098b3
liujie <liujie045.china@outlook.com>
2019-06-04 18:54:18 +0800

    Handy Commit

--- library/src/main/java/com/handy/frame/module/guide/BaseGuideActivity.java
+++ library/src/main/java/com/handy/frame/module/guide/BaseGuideActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.blankj.utilcode.util.LogUtils;
@@ -42,10 +40,6 @@ public abstract class BaseGuideActivity extends FrameActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 去除标题栏
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        // 去除状态栏
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.hf_activity_guide);
         bannerBackground = findViewById(R.id.banner_background);
@@ -94,8 +88,8 @@ public abstract class BaseGuideActivity extends FrameActivity {
         BGALocalImageSize localImageSize = new BGALocalImageSize(1080, 1920, 320, 640);
 
         // 设置数据源
-        bannerBackground.setData(localImageSize, ImageView.ScaleType.CENTER_CROP, backgroundResId);
-        bannerForeground.setData(localImageSize, ImageView.ScaleType.CENTER_CROP, foregroundResId);
+        bannerBackground.setData(localImageSize, ImageView.ScaleType.FIT_XY, backgroundResId);
+        bannerForeground.setData(localImageSize, ImageView.ScaleType.FIT_XY, foregroundResId);
     }
 
     /**
