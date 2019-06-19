720de501d7e78e7d3b98c0d10d2539fd05ef67b4
liujie <liujie045.china@outlook.com>
2019-06-20 00:42:50 +0800

    Handy Commit

---
+++ app/src/main/java/com/handy/frame/app/base/LocalApplication.java
@@ -0,0 +1,19 @@
+package com.handy.frame.app.base;
+
+import com.handy.frame.base.FrameApplication;
+
+/**
+ * FileName
+ *
+ * @author LiuJie https://github.com/Handy045
+ * @description File Description
+ * @date Created in 2019-06-20 00:35
+ * @modified By liujie
+ */
+public class LocalApplication extends FrameApplication {
+
+    @Override
+    public void onCreateHDB() {
+
+    }
+}
