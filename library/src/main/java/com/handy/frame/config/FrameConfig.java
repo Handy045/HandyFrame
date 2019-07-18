bea6b6c0f0a5f761bccaaa4492cabd3c5739a0c3
liujie <liujie045.china@outlook.com>
2019-07-18 17:05:06 +0800

    Handy Commit

--- library/src/main/java/com/handy/frame/config/FrameConfig.java
+++ library/src/main/java/com/handy/frame/config/FrameConfig.java
@@ -18,11 +18,11 @@ public class FrameConfig {
     public static final String PROMPT_PREPARING = "数据准备中，请稍候";
     public static final String PROMPT_SUBMITTING = "数据提交中，请稍候";
 
-    public static final String PROMPT_ERROR_TIMEOUT = "连接超时，请稍后再试";
+    public static final String PROMPT_ERROR_TIMEOUT = "服务连接超时，请稍后再试";
     public static final String PROMPT_ERROR_REQUEST = "请求数据错误，请联系管理员";
-    public static final String PROMPT_ERROR_ANALYSIS = "返回数据解析失败，请联系管理员";
-    public static final String PROMPT_ERROR_RESPONSE = "返回数据错误，请联系管理员";
-    public static final String PROMPT_ERROR_SERVER = "服务器连接失败，请联系管理员";
+    public static final String PROMPT_ERROR_ANALYSIS = "服务返回数据解析失败，请联系管理员";
+    public static final String PROMPT_ERROR_RESPONSE = "服务返回数据错误，请联系管理员";
+    public static final String PROMPT_ERROR_SERVER = "服务异常，请联系管理员";
 
     public static final String PROMPT_EMPTY_RESPONSE = "返回数据为空";
     public static final String PROMPT_EMPTY_RESPONSE_NOMORE = "暂无更多数据";
